9ae4b9f1b5e22b6df7cedb54e7c45230adbd9dec
abelyaev <a_belyaev@icloud.com>
2020-09-10 10:43:15 +0300

    fix mistakes

--- 1L_Беляев_Александр.playground/Contents.swift
+++ 1L_Беляев_Александр.playground/Contents.swift
@@ -18,13 +18,13 @@ if coefficientA == 0 {
 
     let discriminant:Float = (coefficientB*coefficientB)-4*coefficientA*coefficientC
 
-    print ("\nДискриминант (D) =", discriminant)
+    print ("\nДискриминант (D) =", NSString(format: "%.2f", discriminant))
 
     if discriminant > 0 {
         let firstRoot:Float = (-coefficientB + sqrt(discriminant))/(2*coefficientA)
-        print ("\nПервый корень уравнения (X1) =", firstRoot)
+        print ("\nПервый корень уравнения (X1) =", NSString(format: "%.2f", firstRoot))
         let secondRoot:Float = (-coefficientB - sqrt(discriminant))/(2*coefficientA)
-        print ("Второй корень уравнения (X2) =", secondRoot)
+        print ("Второй корень уравнения (X2) =", NSString(format: "%.2f", secondRoot))
     } else if discriminant < 0 {
         print ("\nУравнение не имеет вещественных корней, так как дискриминант меньше 0.")
     } else {
@@ -50,13 +50,13 @@ if cathetusA <= 0 {
     print ("Катет B =", cathetusB)
 
     let hypotenuse:Float = sqrt((cathetusA*cathetusA)+(cathetusB*cathetusB))
-    print ("\nГипотенуза треугольника (C) =", hypotenuse)
-
+    print("\nГипотенуза треугольника (C) =", NSString(format: "%.2f", hypotenuse))
+    
     let square:Float = (cathetusA*cathetusB)/2
-    print ("\nПлощадь треугольника (S) =", square)
+    print ("\nПлощадь треугольника (S) =", NSString(format: "%.2f", square))
 
     let perimeter:Float = cathetusA + cathetusB + hypotenuse
-    print ("Периметр треугольника (P) =", perimeter)
+    print ("Периметр треугольника (P) =", NSString(format: "%.2f", perimeter))
 }
 
 print("\n========================================================================================\n")
@@ -82,7 +82,7 @@ if deposit < 0 {
     repeat {
         let profit:Float = (deposit * (percent / 100))
         deposit = deposit + profit
-        print ("Сумма вклада на конец",index,"-го года :", deposit)
+        print ("Сумма вклада на конец",index,"-го года :", NSString(format: "%.2f", deposit))
         index += 1
     } while index < 6
 }
