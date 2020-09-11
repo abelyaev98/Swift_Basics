67491911dddd75fa72c7ecbfebd3b6b8e76a924c
abelyaev <a_belyaev@icloud.com>
2020-09-11 17:09:17 +0300

    complete task 2 and 3

--- 2L_Беляев_Александр.playground/Contents.swift
+++ 2L_Беляев_Александр.playground/Contents.swift
@@ -1,9 +1,43 @@
 import UIKit
 
-var сheckedNumber:Int = 12
+//Задание 1. Написать функцию, которая определяет, четное число или нет.
 
-if сheckedNumber % 2 == 0 {
-    print ("Число \(сheckedNumber) - четное")
-} else {
-    print ("Число \(сheckedNumber) - нечетное")
+func evenNotEven (_ сheckedNumber: Int) {
+    if сheckedNumber % 2 == 0 {
+        print ("\nЧисло \(сheckedNumber) - четное.")
+    } else {
+        print ("\nЧисло \(сheckedNumber) - нечетное.")
+    }
 }
+
+evenNotEven(20)
+
+print("\n========================================================================================\n")
+
+//Задание 2. Написать функцию, которая определяет, делится ли число без остатка на 3.
+
+func divisionByThree (_ сheckedNumber: Int) {
+    if сheckedNumber % 3 == 0 {
+        print ("Число \(сheckedNumber) - делится на 3 без остатка.")
+    } else {
+        print ("Число \(сheckedNumber) - не делится на 3 без остатка.")
+    }
+}
+
+divisionByThree(33)
+
+print("\n========================================================================================\n")
+
+//Задание 3. Создать возрастающий массив из 100 чисел.
+
+func fillingTheArray(_ amountOfElements:Int) {
+    let numbers = Array (1...amountOfElements)
+    print ("Исходный массив: \(numbers)")
+}
+
+fillingTheArray(100)
+
+print("\n========================================================================================\n")
+
+// Задание 4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+
